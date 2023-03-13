5fb340354e693ced3493757ab263bf195c047ab8
Akankshita Dash <akankshitadash@yahoo.com>
2023-03-12 19:01:19 -0700

    split files into train test val

--- datasets/scripts/split.py
+++ datasets/scripts/split.py
@@ -1,33 +1,32 @@
 import json
 import random
+import os
+split_files = ['downsampled_images/images.json', 'images/images.json']
+for file in split_files:
+    with open(file, 'r') as f:
+        data = json.load(f)
 
-# Load the JSON data
-with open('../downsampled_images/images.json', 'r') as f:
-    data = json.load(f)
+    data=data['images']
+    random.shuffle(data)
 
-# Shuffle the data
-data=data['images']
-random.shuffle(data)
+    num_entries = len(data)
+    train_split = int(num_entries * 0.8)
+    val_split = int(num_entries * 0.9)
+    train_data = data[:train_split]
+    val_data = data[train_split:val_split]
+    test_data = data[val_split:]
 
-# Split the data
-num_entries = len(data)
-train_split = int(num_entries * 0.8)
-val_split = int(num_entries * 0.9)
-train_data = data[:train_split]
-val_data = data[train_split:val_split]
-test_data = data[val_split:]
+    # Add a "split" key to each entry
+    for entry in train_data:
+        entry['split'] = 'train'
+        entry['caption']['raw'].replace('                                                 ','')
+    for entry in val_data:
+        entry['split'] = 'val'
+        entry['caption']['raw'].replace('                                                ','')
+    for entry in test_data:
+        entry['split'] = 'test'
+        entry['caption']['raw'].replace('                                                ','')
 
-# Add a "split" key to each entry
-for entry in train_data:
-    entry['split'] = 'train'
-    entry['caption']['raw'].replace('                                                 ','')
-for entry in val_data:
-    entry['split'] = 'val'
-    entry['caption']['raw'].replace('                                                ','')
-for entry in test_data:
-    entry['split'] = 'test'
-    entry['caption']['raw'].replace('                                                ','')
-
-# Write the split data to separate JSON files
-with open('../downsampled_images/split_images.json', 'w') as f:
-    json.dump({'images':train_data+test_data+val_data}, f)
+    # Write the split data to separate JSON files
+    with open(file, 'w') as f:
+        json.dump({'images':train_data+test_data+val_data}, f)
