50886e8be654623cb82d610e2e742c186bd817d4
Akankshita Dash <akankshitadash@yahoo.com>
2023-03-14 01:01:05 -0700

    remove punctuation and refactor code

--- qualitative_analysis/analysis.py
+++ qualitative_analysis/analysis.py
@@ -3,100 +3,102 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import nltk
 from collections import Counter
-
-nltk.download('stopwords')
+import re
 from nltk.corpus import stopwords
+nltk.download('stopwords')
 
 context_densenet = pd.read_csv('context_densenet.csv')
 context_resnet = pd.read_csv('context_resnet.csv')
 no_context_resnet = pd.read_csv('no_context_resnet.csv')
 no_context_densenet = pd.read_csv('no_context_densenet.csv')
 
-dataset = "no_context_resnet"
-datasetChartTitle = "Context ResNet"
-lst = no_context_resnet.label_generated.values.tolist()
+datasets = {'context_densenet': context_densenet,
+            'context_resnet': context_resnet,
+            'no_context_resnet': no_context_resnet,
+            'no_context_densenet': no_context_densenet}
 
-# Calculate distribution of lengths
+regex_extract_word = r'\b\w+\b'
+for dataset in datasets.keys():
+    # dataset = "no_context_resnet"
+    # datasetChartTitle = dataset.split("_")[:-1]+dataset.split("_")[-1]
+    split_tokens = dataset.split("_")
+    datasetChartTitle = dataset.split("_")[0] + " " + dataset.split("_")[-1] if len(split_tokens)==2 else \
+        dataset.split("_")[0]+" " + dataset.split("_")[1] + " " + dataset.split("_")[-1]
+    lst = datasets[dataset].label_generated.values.tolist()
 
-print("List ", lst)
-summary_lengths = list(map(len, lst))
-print("Generated lengths ", summary_lengths)
+    # Calculate distribution of lengths
+    # print("List ", lst)
+    summary_lengths = list(map(len, lst))
+    # print("Generated lengths ", summary_lengths)
+    sns.histplot(summary_lengths)
+    plt.title('Length Distributions for ' + datasetChartTitle + ' dataset')
+    plt.xlabel('Summary lengths')
+    plt.ylabel('Number of summaries')
+    # plt.show()
+    plt.savefig(dataset + '_lengthdist.png')
+    plt.close()
 
-sns.histplot(summary_lengths)
-plt.title('Length Distributions')
-plt.xlabel('Summary lengths')
-plt.ylabel('Number of summaries')
-# plt.show()
-plt.savefig(dataset + '_lengthdist.png')
+    # Plot most common words (possibly with and without stopwords - see which one is the most informative!
+    # Stopwords are common words that don't add information - e.g. "and", "the"
 
-# Plot most common words (possibly with and without stopwords - see which one is the most informative!
-# Stopwords are common words that don't add information - e.g. "and", "the"
+    cnt = Counter()
+    cnt_bigrams = Counter()
+    cnt_trigrams = Counter()
 
-cnt = Counter()
+    stop_words = set(stopwords.words('english'))
+    for text in lst:
+        words = re.findall(regex_extract_word, text)
+        for i in words:
+            if i.lower() not in stop_words:
+                cnt[i] += 1
+        for i in range(0, len(words) - 1):
+            cnt_bigrams[words[i] + ' ' + words[i + 1]] += 1
+        for i in range(0, len(words) - 2):
+            cnt_trigrams[words[i] + ' ' + words[i + 1] + ' ' + words[i + 2]] += 1
+    # See most common ten words
+    print("**** DATASET {} ****".format(dataset))
+    print("Most common words ", cnt.most_common(10))
+    word_freq = pd.DataFrame(cnt.most_common(15), columns=['words', 'count'])
+    word_freq.head()
+    fig, ax = plt.subplots(figsize=(12, 8))
+    # Plot horizontal bar graph
+    word_freq.sort_values(by='count').plot.barh(x='words',
+                          y='count',
+                          ax=ax,
+                          color="green")
+    ax.set_title("Most Common Unigrams")
+    plt.savefig(dataset + '_unigrams.png')
+    plt.close()
 
-stop_words = set(stopwords.words('english'))
+    print("Most common bigrams ", cnt_bigrams.most_common(10))
+    word_freq = pd.DataFrame(cnt_bigrams.most_common(15),
+                                 columns=['words', 'count'])
+    word_freq.head()
 
-for text in lst:
-    for word in text.split():
-        if word.lower() not in stop_words:
-            cnt[word] += 1
-# See most common ten words
-print("Most common words ", cnt.most_common(10))
-word_freq = pd.DataFrame(cnt.most_common(15),
-                             columns=['words', 'count'])
-word_freq.head()
+    fig, ax = plt.subplots(figsize=(12, 8))
 
-fig, ax = plt.subplots(figsize=(12, 8))
+    # Plot horizontal bar graph
+    word_freq.sort_values(by='count').plot.barh(x='words',
+                          y='count',
+                          ax=ax,
+                          color="green")
+    ax.set_title("Most Common Bigrams")
+    plt.savefig(dataset + '_bigrams.png')
+    plt.close()
 
-# Plot horizontal bar graph
-word_freq.sort_values(by='count').plot.barh(x='words',
-                      y='count',
-                      ax=ax,
-                      color="green")
-ax.set_title("Most Common Unigrams")
-plt.savefig(dataset + '_unigrams.png')
+    # See most common ten trigrams
+    print("Most common trigrams ", cnt_trigrams.most_common(10))
+    word_freq = pd.DataFrame(cnt_trigrams.most_common(15),
+                                 columns=['words', 'count'])
+    word_freq.head()
 
-# Visualization of top n bigrams
-from collections import Counter
-cnt = Counter()
-from nltk.corpus import stopwords
+    fig, ax = plt.subplots(figsize=(12, 8))
 
-stop_words = set(stopwords.words('english'))
-
-for text in lst:
-    for word in range(0, len(text.split()) - 1):
-        cnt[text.split()[word] + ' ' + text.split()[word + 1]] += 1
-# See most common ten bigrams
-print("Most common bigrams ", cnt.most_common(10))
-word_freq = pd.DataFrame(cnt.most_common(15),
-                             columns=['words', 'count'])
-word_freq.head()
-
-fig, ax = plt.subplots(figsize=(12, 8))
-
-# Plot horizontal bar graph
-word_freq.sort_values(by='count').plot.barh(x='words',
-                      y='count',
-                      ax=ax,
-                      color="green")
-ax.set_title("Most Common Bigrams")
-plt.savefig(dataset + '_bigrams.png')
-
-for text in lst:
-    for word in range(0, len(text.split()) - 2):
-        cnt[text.split()[word] + ' ' + text.split()[word + 1] + text.split()[word + 2]] += 1
-# See most common ten bigrams
-print("Most common trigrams ", cnt.most_common(10))
-word_freq = pd.DataFrame(cnt.most_common(15),
-                             columns=['words', 'count'])
-word_freq.head()
-
-fig, ax = plt.subplots(figsize=(12, 8))
-
-# Plot horizontal bar graph
-word_freq.sort_values(by='count').plot.barh(x='words',
-                      y='count',
-                      ax=ax,
-                      color="green")
-ax.set_title("Most Common Trigrams")
-plt.savefig(dataset + '_trigrams.png')
+    # Plot horizontal bar graph
+    word_freq.sort_values(by='count').plot.barh(x='words',
+                          y='count',
+                          ax=ax,
+                          color="green")
+    ax.set_title("Most Common Trigrams")
+    plt.savefig(dataset + '_trigrams.png')
+    plt.close()
