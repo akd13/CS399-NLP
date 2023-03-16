97d508732322d6656edc2d44d7c41840f607c3ce
Akankshita Dash <akankshitadash@yahoo.com>
2023-03-15 21:21:25 -0700

    rename statista to statista-large

--- datasets/scripts/downsample.py
+++ datasets/scripts/downsample.py
@@ -1,24 +1,24 @@
 import os
 import random
 import shutil
-
+print(os.getcwd())
 # Define the original and new directories
-original_dir = '../images'
-new_dir = '../downsampled_images'
+original_dir = 'statista-large/imgs/'
+new_dir = 'statista/imgs/'
 
 # Define the downsampling factor (1/10th in this case)
-downsampling_factor = 10
+downsampling_factor = 5
 
 # Create the new directory
 os.makedirs(new_dir, exist_ok=True)
 
 # Loop over each dataset
-for dataset, size in [('HCI', 407), ('Concadia', 171), ('Pew', 9285), ('Statista', 34811)]:
+for dataset, size in [('statista-large/imgs', 34811)]:
     # Calculate the number of images to keep from this dataset
     n_keep = size // downsampling_factor
 
     # Get a list of all the filenames from this dataset
-    filenames = [f for f in os.listdir(original_dir) if f.startswith(dataset.lower())]
+    filenames = [f for f in os.listdir(original_dir)]
     print(dataset, len(filenames))
     # Randomly select n_keep filenames from this dataset
     keep_filenames = random.sample(filenames, n_keep)
